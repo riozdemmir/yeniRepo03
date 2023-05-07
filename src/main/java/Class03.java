74ac1e321b46eb666b815f900df3c2109e59bce1
John Doe <JohnDoe@gmail.com>
2023-05-07 13:34:54 +0300

    V4 calismasi

--- src/main/java/Class03.java
+++ src/main/java/Class03.java
@@ -10,6 +10,8 @@ public class Class03 {
         System.out.println(x+y+z);
         //V3
         System.out.println("V3 kodları..");
+        //V4
+        System.out.println("V4 çalışmaları..");
 
 
     }
